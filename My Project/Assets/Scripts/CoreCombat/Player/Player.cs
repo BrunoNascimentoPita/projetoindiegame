3b9bf2a48adea9a8f562ba8f04af6eceb70fc335
Guilherm Vianna <mataveli91@gmail.com>
2022-03-08 19:41:11 -0300

    Implementação do Scriptable do Player

--- My Project/Assets/Scripts/CoreCombat/Player/Player.cs
+++ My Project/Assets/Scripts/CoreCombat/Player/Player.cs
@@ -13,32 +13,38 @@ namespace WindRose.PlayerContent
         [SerializeField] Rigidbody2D _rb;
         [SerializeField] Animator _ani;
         [SerializeField] BoxCollider2D _attackBox;
+        [SerializeField] PlayerValues PlayerDataBase;
 
         public static bool OnGrounded;
         private static readonly int IsStopped = Animator.StringToHash("IsStopped");
 
         public static int Life;
 
+        float attackCoolDown;
+
         void Start()
         {
-            PlayerDataBase Data = new();
-            _lifeMax = Data.MAXLIFE;
+            GetVariables();
         }
+
+
         void Update()
         {
+
             Move();
 
             if (Input.GetKeyDown(KeyCode.Space) && OnGrounded)
-                Jump();
-
-            if (Input.GetKeyDown(KeyCode.F) && OnGrounded)
-                _ani.SetTrigger("IsAttacking");
-
-            if (Input.GetKeyDown(KeyCode.Alpha1))
             {
-                /*  LoadVariables(); */
-                print("Atributos Carregados");
+                Jump();
             }
+
+            if (Input.GetKeyDown(KeyCode.F) && OnGrounded && attackCoolDown > _attackSpeed)
+            {
+                _ani.SetTrigger("IsAttacking");
+                attackCoolDown = 0;
+            }
+
+            attackCoolDown += Time.deltaTime;
         }
 
         void LateUpdate()
@@ -71,17 +77,14 @@ namespace WindRose.PlayerContent
             _rb.AddForce(new Vector2(0, _jumpForce));
         }
 
+        void GetVariables()
+        {
+            _lifeMax = PlayerDataBase.VidaMaxima;
+            _lifeActual = _lifeMax;
 
-    }
-    [System.Serializable]
-    class PlayerDataBase
-    {
-        public int MAXLIFE = 5;
-        public int SPEED = 3;
-        public float JUMPFORCE = 200;
-    }
-    class PlayerFunctions
-    {
-
+            _speed = PlayerDataBase.Velocidade;
+            _attackSpeed = PlayerDataBase.AtaqueVelocidade;
+            _jumpForce = PlayerDataBase.ForcaPulo;
+        }
     }
 }
